812881c5008453a2d6c3a1a6f13400ac96a2b112
Dan Danilyuk and Sarah Lindmar <dan@>
2017-09-06 14:16:09 -0700

    overwrite text file with replace

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -6,6 +6,8 @@ class Replace
   def magic(text, find, replace)
     changed = text.gsub!(find, replace)
     puts changed
+    sample = File.open("../simple_file.txt",  "w+")
+    sample.puts(changed)
     return changed
   end
 end
@@ -14,10 +16,13 @@ example = Replace.new()
 # commented out per simple_file.txt reading
 # puts "Enter sentence you want to modify."
 # # text = gets.chomp()
-text = File.read("simple_file.txt")
+text = File.read("../simple_file.txt")
 puts "Enter the word you want to find."
 find = gets.chomp()
 puts "Enter the word you want to replace find with"
 replace = gets.chomp()
 
 example.magic(text, find, replace)
+
+
+# File.read("simple_file.txt")
