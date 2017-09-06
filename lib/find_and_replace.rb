7ec6858c6e8509245272c2aca676998e76ad3a19
Dan Danilyuk and Sarah Lindmar <dan@>
2017-09-06 12:03:47 -0700

    make script execuatable

--- lib/find_and_replace.rb
+++ lib/find_and_replace.rb
@@ -1,9 +1,20 @@
+#!/usr/bin/env ruby
 class Replace
   def Initialize()
   end
 
   def magic(text, find, replace)
     changed = text.gsub!(find, replace)
-    return changed
+    puts changed
   end
 end
+
+example = Replace.new()
+puts "Enter sentence you want to modify."
+text = gets.chomp()
+puts "Enter the word you want to find."
+find = gets.chomp()
+puts "Enter the word you want to replace find with"
+replace = gets.chomp()
+
+example.magic(text, find, replace)
